17f00662f0b31f314dfc640e3ce180bd697e3e5a
georgeli <george.li@gliacloud.com>
2017-05-12 16:18:44 +0800

    update

--- topbuzz/run.sh
+++ topbuzz/run.sh
@@ -8,6 +8,6 @@
 
 
 echo $1 $2
-docker run -it --rm -v `pwd`:/work -w /work markadams/chromium-xvfb-py2 python topbuzz.py $1 $2 tmp
+docker run --rm -v `pwd`:/work -w /work markadams/chromium-xvfb-py2 python topbuzz.py $1 $2 tmp
 mkdir -p $1
 mv tmp `date +$1/%Y-%m-%d.csv`
